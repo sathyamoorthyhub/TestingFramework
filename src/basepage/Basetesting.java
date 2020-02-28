85261ca9c39997b1e906385c613e4a383faa4530
Sathyamoorthy Palanisamy <sathyamoorthy.p@sensiple.com>
2020-02-28 13:24:25 +0530

    after

--- src/basepage/Basetesting.java
+++ src/basepage/Basetesting.java
@@ -341,7 +341,7 @@ public class Basetesting implements IRetryAnalyzer,IAnnotationTransformer,ITestL
 
 
 		
-		System.out.println("LinkedHashMap sssssssssssssss  ");
+		System.out.println("LinkedHashMap are  ");
 
 
 
