4bd1201cb1004e957c1e7f272094bf27ff8cac0c
Sathyamoorthy Palanisamy <sathyamoorthy.p@sensiple.com>
2020-02-28 12:27:11 +0530

    rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr

--- src/basepage/Basetesting.java
+++ src/basepage/Basetesting.java
@@ -340,19 +340,12 @@ public class Basetesting implements IRetryAnalyzer,IAnnotationTransformer,ITestL
 	    Iterator<Integer> iteratorr=it1.iterator();
 	
 
-
-
-		
-<<<<<<< HEAD
-
-
-
 		
 		System.out.println("LinkedHashMapppppp  ");
 
-=======
-		System.out.println("LinkedHashMap  ");
->>>>>>> parent of bc9acca... testscommitttttttttttttttttttttttttttttttt
+
+	
+
 
 
 		while(iteratorr.hasNext())
