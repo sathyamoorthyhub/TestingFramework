8778a0af45091dbcd90baadfdebfda0061812972
Sathyamoorthy Palanisamy <sathyamoorthy.p@sensiple.com>
2020-02-28 12:46:52 +0530

    Revert "Revert "rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr""
    This reverts commit 97c7a8f92e0fee09742c507005359684e020aa2c.

--- src/basepage/Basetesting.java
+++ src/basepage/Basetesting.java
@@ -341,7 +341,7 @@ public class Basetesting implements IRetryAnalyzer,IAnnotationTransformer,ITestL
 	
 
 		
-		System.out.println("LinkedHashMap LinkedHashMap Testing   ");
+		System.out.println("LinkedHashMap LinkedHashMap Testiiiiiiiiiiiiiiiiiiiiiiing   ");
 
 
 
