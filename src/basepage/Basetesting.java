fa1f0629830f52fa2a5d8e9cb5f6af4bebf363b1
Sathyamoorthy Palanisamy <sathyamoorthy.p@sensiple.com>
2020-03-09 13:22:07 +0530

    cccccccccccccccccccciiiiiiiiiiiiiiiiii

--- src/basepage/Basetesting.java
+++ src/basepage/Basetesting.java
@@ -370,7 +370,7 @@ public class Basetesting implements IRetryAnalyzer,IAnnotationTransformer,ITestL
 		Set<String> it1=lh1.keySet();
 	    Iterator<String> iteratorr=it1.iterator();
 	
-		System.out.println("LinkedHashMap mapppppppppppp ");
+		System.out.println("LinkedHashMap mapppppppppppppppppppppp ");
 		System.out.println("LinkedHashMap::");
 				
 		while(iteratorr.hasNext())
